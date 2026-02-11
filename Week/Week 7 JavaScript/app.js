f616d5613dc6597ecfb4c566a4fdd4fdffcdcd24
Santi Panyanoi <aon007567@gmail.com>
2026-02-11 11:20:00 +0700

    Add files via upload

---
+++ Week/Week 7 JavaScript/app.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    alert("Hello! This is a message from JavaScript.");
+}
+document.getElementById("btn").addEventListener("click", showMessage);
+
+console.log("JavaScript is running!");
